cd721dfa31d99cefc99d84a0437dc1b7d6bdf597
bmgrether <bmgrether@gmail.com>
2017-08-20 02:10:53 +0000

    Implemented edit and update functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,4 +19,22 @@ class ProjectsController < ApplicationController
         end
     end
     
+    def edit
+        @project_item = Project.find(params[:id])
+    end
+    
+    def update
+        @project_item = Project.find(params[:id])
+        
+        respond_to do |format|
+            if @project_item.update(params.require(:project).permit(:title, :subtitle, :body))
+                format.html { redirect_to projects_path, notice: 'The record successfully updated.' }
+                format.json { render :show, status: :ok, location: @project }
+            else
+                format.html { render :edit }
+                format.json { render json: @project.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+    
 end
