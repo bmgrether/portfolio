08684fbba666a48a08261ad8accdd4068d17c329
bmgrether <bmgrether@gmail.com>
2017-11-24 00:16:04 +0000

    Updated projects to include attr for position

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
     access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
     
     def index
-        @project_items = Project.all
+        @project_items = Project.by_position
     end
     
     def new
