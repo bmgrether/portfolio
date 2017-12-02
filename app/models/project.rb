abe0e3ffdfbb55345c7ed6ddecdcb411e5114e5e
bmgrether <bmgrether@gmail.com>
2017-12-02 18:25:11 +0000

    Implemented initial file uploading system via carrierwave for project items, uploaded background video

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,9 @@ class Project < ApplicationRecord
     include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
     
+    mount_uploader :thumb_image, ProjectUploader
+    mount_uploader :main_image, ProjectUploader
+    
     def self.ruby_on_rails
         where(subtitle: "Ruby on Rails")
     end
